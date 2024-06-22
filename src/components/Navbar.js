b82717c4ee44f8c89a079bf8e428aa6cd9a0f468
Julian-Lafond <jmlafond@ucsc.edu>
2024-06-22 14:51:42 -0700

    add sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,42 @@
+import {useState } from "react"
+import Sidebar from "./Sidebar"
+
 export default function Navbar(){
+    const [showSideBar, setShowSidebar] = useState(false)
+    const links = [
+        {
+            name: "Home",
+            path: "/"
+        },
+        {
+            name: "Recipes",
+            path: "/recipes"
+        },
+        {
+            name: "Settings",
+            path: "/settings"
+        }
+    ]
     return(
+        <>
         <div className = "navbar container">
             <a href = "#!" className="logo">F<span>oo</span>diesHub</a>
             <div className="nav-links">
-                <a href = "#!">Home</a>
+                { links.map(link => (
+                    <a href = "#!" key = {link.name}>{link.name}</a>
+                ))}
+                
+                {/*<a href = "#!">Home</a>
                 <a href = "#!">Recipes</a>
-                <a href = "#!">Settings</a>
+                <a href = "#!">Settings</a>*/}
             </div>
-            <div className="sidebar-btn">
+            <div onClick = {() => setShowSidebar(!showSideBar)} className={showSideBar ? "sidebar-btn active" : "sidebar-btn"}>
                 <div className="bar"></div>
                 <div className="bar"></div>
                 <div className="bar"></div>
             </div>
         </div>
+        <Sidebar links={links}/>
+        </>
     )
 }
