a53312dbf26f8483eb6e0651a9ef09c8319867c6
Julian-Lafond <jmlafond@ucsc.edu>
2024-06-24 14:17:53 -0700

    finished sidebar

---
+++ src/components/HeroSection.js
@@ -0,0 +1,14 @@
+export default function HeroSection(){
+    return(
+        <div className="section">
+            <div className="col">
+                <h1 className="title">What Are We About</h1>
+                <p className="info">FoodiesHub is a place where you can please your soul and tummy with delicious food recepies of all cuisine. And our service is absolutely free. So start exploring now.</p>
+                <button className="btn">Explore Now</button> 
+            </div>
+            <div className="col">
+                col 2
+            </div>
+        </div>
+    )
+}
