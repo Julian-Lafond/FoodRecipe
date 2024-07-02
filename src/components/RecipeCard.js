768ccc8d072b688f33a90427dca5a605ada02b55
Julian-Lafond <jmlafond@ucsc.edu>
2024-07-02 13:10:46 -0700

    finished recipes page

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,9 +1,15 @@
 import CustomImage from "./CustomImage"
 
-export default function RecipeCard(){
+export default function RecipeCard({recipe}){
     return(
         <div className="recipe-card">
-            <CustomImage imgSrc="/img/gallery/img_1.jpg" pt="65%" />
+            <CustomImage imgSrc= {recipe.image} pt="65%" />
+            <div className="recipe-card-info">
+                <img className = "auther-img" src = {recipe.authorImg} alt = "" />
+                <p className = "recipe-title">{recipe.title}</p>
+                <p className="recipe-desc">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <a className ="view-btn" href ="#!">VIEW RECIPE</a>
+            </div>
         </div>
     )
     }
